05a8227f94ad37d5e71fa32b5367319636408cb2
Kevin Liang <kevinliang0430@gmail.com>
2023-01-03 13:44:32 +1300

    changed recipient list to contain std::string

--- RecipientsList.hh
+++ RecipientsList.hh
@@ -1,9 +1,19 @@
 #ifndef RECIPIENTS_LIST_H
 #define RECIPIENTS_LIST_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 class RecipientsList
 {
+public:
+    RecipientsList(std::vector<std::string> recipients);
+    
+    void addRecipient(std::string recipient);
 
+private:
+    std::vector<std::string> recipients;
 };
 
 #endif
